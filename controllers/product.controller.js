5d2788c8d4f80f822a77de99a8f8b4c3d443b364
jitesh <jit12esh.js@gmail.com>
2018-09-24 00:36:18 +0530

    crud Module

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -22,3 +22,24 @@ exports.product_create = function (req, res) {
         res.send('Product Created successfully')
     })
 };
+
+exports.product_details = function (req, res) {
+    Product.find(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Product udpated.');
+    });
+};
+
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
